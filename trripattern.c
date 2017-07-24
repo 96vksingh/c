1022d8526a4569377255b0e5f7ba2633ce0ff9e4
96vksingh <01vksingh@gmail.com>
2017-07-24 17:20:27 +0530

    trrryy,,,,,patern

--- trripattern.c
+++ trripattern.c
@@ -1,35 +1,24 @@
 #include<stdio.h>
-main()
-{
-  int i,j,k,a,h,d,l,u;
-  printf("HOW MUCH LONG PATTERN U WANT(ODD)-");
-  scanf("%d", &a);
-
-  for(i=1;i<=(a/2)+1;i++)
+#include<stdlib.h>
+    int main()
     {
-      l=(a/2)+1; j=a;
-      u=(a/2)+1;
-      d=(a/2)+1;
-      for(h=1;h<=a;h++)
-       {
-          if(i%2==0)
+       int a,c=0,i,j,d;
+       printf("HOW MUCH LONG PATTERN U WANT(ODD)-");
+       scanf("%d", &a);
+       for(i=(a/2)+1;i>=1;i--)
           {
-            if (h==d+1 || h==u+1)
-            printf("*");
-            else
-            printf("\t");
-            d--; u++;
-          }
-          else
-          {
-          if(h==d || h==u || h==l)
-          printf("*");
-          else
-          printf("\t");
-          d-=2; u+=2;
+            c=c+1;
+               for(j=1;j<=a;j++)
+                 {
+               if(j==i)
+                {
+                  for(d=1;d<=(2*c)-1;d+=2)
+                   printf("* ");
+                }
+                else
+                printf(" ");
+             }
+             printf("\n");
         }
-      }
-      printf("\n");
-      }
-
+        return 0;
     }
